7c66239f4bee17a47e0666a759c253853c9b872c
manhabasher <manha1205@gmail.com>
2025-02-28 15:37:47 -0500

    task is a class i will use in planner

---
+++ task.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+
+public class task{
+    String title;
+    String description;
+    boolean isCompleted;
+    LocalDateTime DueDate;
+task(){
+    this.title = title;
+    this.description = description;
+    this.isCompleted = false;
+}
+// getters for accessing each variable
+
+}
